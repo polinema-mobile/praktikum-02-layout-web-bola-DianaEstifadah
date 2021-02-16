a685e93ba63dc8740d1c79632224a6b690df4400
Diana Estifadah <dianaesht@gmail.com>
2021-02-16 12:43:41 +0700

    Pratikum Container Property Padding

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,22 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-//Pratikum Container Property Margin
+//Pratikum Container Property Padding
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(title: Text("Contoh Margin")),
+          appBar: AppBar(title: Text("Contoh Padding")),
           body: Container(
-              margin: EdgeInsets.all(50),
+              padding: EdgeInsets.only(left: 20),
+              margin: EdgeInsets.all(20),
               height: 200,
               width: 200,
               alignment: Alignment.topLeft,
               color: Colors.blueGrey,
               child: Text(
-                'Semangat Belajar Flutter',
+                'Ayo Belajar Flutter',
                 style: TextStyle(fontSize: 20, color: Colors.white),
               ))),
     );
