8852190746013f778187e023789efc80d73a66fa
Diana Estifadah <dianaesht@gmail.com>
2021-02-16 12:38:08 +0700

    Pratikum Container Property Color

--- lib/main.dart
+++ lib/main.dart
@@ -2,20 +2,22 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-//Pratikum Container Property Alignment
+//Pratikum Container Property Color
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(title: Text("Contoh Aligment")),
+          appBar: AppBar(title: Text("Contoh Color")),
           body: Container(
-              alignment: Alignment.bottomCenter,
+              margin: EdgeInsets.all(50),
+              height: 200,
+              width: 200,
+              alignment: Alignment.center,
+              color: Colors.amber[900],
               child: Text(
                 'Semangat Belajar',
-                style: TextStyle(
-                  fontSize: 20,
-                ),
+                style: TextStyle(fontSize: 20, color: Colors.white),
               ))),
     );
   }
