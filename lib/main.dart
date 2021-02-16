f501b3edf856cf04e378fcd42a71a78aeb1fce3f
Diana Estifadah <dianaesht@gmail.com>
2021-02-16 12:41:09 +0700

    Pratikum Container Property Height dan Width

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,19 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-//Pratikum Container Property Color
+//Pratikum Container Property Height dan Width
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(title: Text("Contoh Color")),
+          appBar: AppBar(title: Text("Contoh Height dan Width")),
           body: Container(
               margin: EdgeInsets.all(50),
               height: 200,
               width: 200,
               alignment: Alignment.center,
-              color: Colors.amber[900],
+              color: Colors.blueGrey,
               child: Text(
                 'Semangat Belajar',
                 style: TextStyle(fontSize: 20, color: Colors.white),
