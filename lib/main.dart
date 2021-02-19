b74cf0db69b8e60939d7c66f54ee2acd2eea3965
Diana Estifadah <dianaesht@gmail.com>
2021-02-19 08:50:53 +0700

    Pratikum ListView Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,40 @@
 import 'package:flutter/material.dart';
 
+//Pratikum ListView Widget
 void main() => runApp(MyApp());
-
-//Pratikum Stack Widget
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Contoh Stack Widget"),
+          title: Text("Contoh ListView Widget"),
         ),
-        body: Stack(
+        body: ListView(
           children: <Widget>[
             Container(
               color: Colors.green,
-              alignment: Alignment.bottomCenter,
-              child: Text("Satu",
+              alignment: Alignment.topLeft,
+              child: Text(
+                  "ListView widget digunanakan untuk menampilkan data dalam bentuk list dan jika datanya melebihi dari render box maka halaman tersebut dapat di scroll.",
                   style: TextStyle(fontSize: 30, color: Colors.white)),
             ),
             Container(
               color: Colors.red,
-              alignment: Alignment.bottomCenter,
-              child: Text("Dua",
+              alignment: Alignment.topLeft,
+              child: Text(
+                  "ListView widget digunanakan untuk menampilkan data dalam bentuk list dan jika datanya melebihi dari render box maka halaman tersebut dapat di scroll.",
                   style: TextStyle(fontSize: 30, color: Colors.white)),
               height: 400.0,
               width: 300.0,
             ),
             Container(
               color: Colors.deepPurple,
-              alignment: Alignment.bottomCenter,
-              child: Text("Tiga",
+              alignment: Alignment.topLeft,
+              child: Text(
+                  "ListView widget digunanakan untuk menampilkan data dalam bentuk list dan jika datanya melebihi dari render box maka halaman tersebut dapat di scroll.",
                   style: TextStyle(fontSize: 30, color: Colors.white)),
               height: 200.0,
               width: 200.0,
