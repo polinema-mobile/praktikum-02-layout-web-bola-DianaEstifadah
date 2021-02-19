22cbf525f3b3de129e7dd19e6c8955b5c48b5243
Diana Estifadah <dianaesht@gmail.com>
2021-02-19 08:39:32 +0700

    Pratikum Column Widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,30 +2,36 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-//Pratikum Container Property Decoration
+//Pratikum Column Widget
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(title: Text("Contoh Configuration")),
-          body: Container(
-              decoration: BoxDecoration(
-                color: const Color(0xff7c94b6),
-                image: const DecorationImage(
-                  image: NetworkImage(
-                      'https://pixnio.com/free-images/2018/12/02/2018-12-02-19-17-12-1200x801.jpg'),
-                  fit: BoxFit.fitWidth,
-                ),
-                border: Border.all(
-                  color: Colors.black,
-                  width: 8,
-                ),
-                borderRadius: BorderRadius.circular(12),
+        appBar: AppBar(title: Text("Contoh Column Widget")),
+        body: Column(
+          children: [
+            Container(
+              color: Colors.purpleAccent,
+              child: FlutterLogo(
+                size: 90.0,
               ),
-              height: 200,
-              width: 300,
-              margin: EdgeInsets.all(20))),
+            ),
+            Container(
+              color: Colors.greenAccent,
+              child: FlutterLogo(
+                size: 90.0,
+              ),
+            ),
+            Container(
+              color: Colors.orangeAccent,
+              child: FlutterLogo(
+                size: 90.0,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
