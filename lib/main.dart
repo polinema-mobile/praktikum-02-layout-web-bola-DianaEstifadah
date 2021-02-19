fe748ea28c6d13e4913f68468c0a1d1f0bfd91ca
Diana Estifadah <dianaesht@gmail.com>
2021-02-19 08:23:05 +0700

    Pratikum Container Property Transform

--- lib/main.dart
+++ lib/main.dart
@@ -2,24 +2,30 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-//Pratikum Container Property Padding
+//Pratikum Container Property Transform
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(title: Text("Contoh Padding")),
+          appBar: AppBar(title: Text("Contoh Transform")),
           body: Container(
-              padding: EdgeInsets.only(left: 20),
-              margin: EdgeInsets.all(20),
-              height: 200,
-              width: 200,
-              alignment: Alignment.topLeft,
-              color: Colors.blueGrey,
-              child: Text(
-                'Ayo Belajar Flutter',
-                style: TextStyle(fontSize: 20, color: Colors.white),
-              ))),
+            decoration: BoxDecoration(
+              image: const DecorationImage(
+                image: NetworkImage(
+                    'https://upload.wikimedia.org/wikipedia/commons/8/8d/American_bison_k5680-1.jpg'),
+                fit: BoxFit.fitWidth,
+              ),
+              border: Border.all(
+                color: Colors.black,
+                width: 8,
+              ),
+            ),
+            height: 200,
+            width: 300,
+            margin: const EdgeInsets.only(left: 30.0, right: 30.0, top: 30),
+            transform: Matrix4.rotationZ(-0.1),
+          )),
     );
   }
 }
